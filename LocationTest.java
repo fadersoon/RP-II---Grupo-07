bb1f77ff191266e02da0eb06d9abc7f456b4ebab
Rodrigo Thoma da Silva <7662917@unipampa.edu.br>
2025-09-07 20:32:40 -0300

    Edit LocationTest.java

--- LocationTest.java
+++ LocationTest.java
@@ -1,21 +1,29 @@
 package luxus;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class LocationTest {
+class LocationTest {
 
     @Test
     void testCriacaoEGetters() {
+        Location loc = new Location(15, 30);
+        assertEquals(15, loc.getX(), "O getter de X retornou o valor errado.");
+        assertEquals(30, loc.getY(), "O getter de Y retornou o valor errado.");
+    }
 
-        Location loc = new Location(-15, -30);
+    @Test
+    void testConstrutorDeveLancarExcecaoParaXNegativo() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Location(-1, 10);
+        });
+    }
 
-        assertEquals(-15, loc.getX(), "O getter de X retornou o valor errado.");
-        assertEquals(-30, loc.getY(), "O getter de Y retornou o valor errado.");
+    @Test
+    void testConstrutorDeveLancarExcecaoParaYNegativo() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Location(10, -1);
+        });
     }
 }
     
