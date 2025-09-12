86ba73924431915fe3ce156ac1c219a5014b68f2
rodrigo thoma <rodrigothdasilva@gmail.com>
2025-09-12 19:58:15 -0300

    Refatoração nas classe MissingPassengerException e PassengerSource

--- luxusproject/src/main/java/luxusproject/MissingPassengerException.java
+++ luxusproject/src/main/java/luxusproject/MissingPassengerException.java
@@ -4,26 +4,22 @@ public class MissingPassengerException extends RuntimeException {
 
     private final Vehicle vehicle;
 
-    // Construtor principal: com veículo e mensagem padrão
     public MissingPassengerException(Vehicle vehicle) {
         super("Missing passenger at pickup location for vehicle: "
                 + (vehicle != null ? vehicle.getId() : "unknown"));
         this.vehicle = vehicle;
     }
 
-    // Construtor alternativo: com veículo e mensagem customizada
     public MissingPassengerException(Vehicle vehicle, String message) {
         super(message);
         this.vehicle = vehicle;
     }
 
-    // Construtor alternativo: com veículo, mensagem e causa
     public MissingPassengerException(Vehicle vehicle, String message, Throwable cause) {
         super(message, cause);
         this.vehicle = vehicle;
     }
 
-    // Getter
     public Vehicle getVehicle() {
         return vehicle;
     }
--- luxusproject/src/main/java/luxusproject/PassengerSource.java
+++ luxusproject/src/main/java/luxusproject/PassengerSource.java
@@ -1,8 +1,5 @@
 package luxusproject;
 
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Random;
 
 public class PassengerSource implements Actor {
