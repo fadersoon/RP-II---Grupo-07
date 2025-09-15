82e86f8afb3969ed6c6bcdc5fe685dc8af939466
rodrigo thoma <rodrigothdasilva@gmail.com>
2025-09-15 19:28:27 -0300

    Ajuste de pacote

---
+++ luxusproject/src/test/java/luxusproject/LocationTest.java
@@ -0,0 +1,30 @@
+package luxusproject;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class LocationTest {
+
+    @Test
+    void testCriacaoEGetters() {
+        Location loc = new Location(15, 30);
+        assertEquals(15, loc.getX(), "O getter de X retornou o valor errado.");
+        assertEquals(30, loc.getY(), "O getter de Y retornou o valor errado.");
+    }
+
+    @Test
+    void testConstrutorDeveLancarExcecaoParaXNegativo() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Location(-1, 10);
+        });
+    }
+
+    @Test
+    void testConstrutorDeveLancarExcecaoParaYNegativo() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Location(10, -1);
+        });
+    }
+}
+    
+     
