7f5856705b51b85d534afeed384ae49d5d894a14
Gustavo Fernandes dos Anjos <gustavoanjos.aluno@unipampa.edu.br>
2025-09-17 10:50:05 -0300

    Criei test

---
+++ luxusproject/src/test/java/luxusproject/LuxCompanyTest.java
@@ -0,0 +1,16 @@
+package luxusproject;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LuxCompanyTest {
+
+    @Test
+    void requestPickup() {
+    }
+
+    @Test
+    void arrivedAtPickup() {
+    }
+}
