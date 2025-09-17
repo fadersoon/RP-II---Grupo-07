3c8f33245824d192ae48b8b4040953ec35bb3b53
Gustavo Fernandes dos Anjos <gustavoanjos.aluno@unipampa.edu.br>
2025-09-17 11:44:55 -0300

    Teste dos métodos requestPickup/arriviedAtPickup

--- luxusproject/src/test/java/luxusproject/LuxCompanyTest.java
+++ luxusproject/src/test/java/luxusproject/LuxCompanyTest.java
@@ -1,15 +1,35 @@
 package luxusproject;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 
 class LuxCompanyTest {
 
     @Test
-    void requestPickup() {
+    void testarSeACompanhiaIniciaCom4Veiculos() {
+
+        City cidade = new City(100,100);
+        LuxCompany empresa = new LuxCompany(cidade);
+        assertEquals(4,empresa.getVehicles().size(),"erro");
+
+
     }
 
     @Test
-    void arrivedAtPickup() {
+    void testarSeRequestPickupFuncionaComVeiculoLivre() {
+        City cidade = new City(100,100);
+        LuxCompany empresa = new LuxCompany(cidade);
+
+        Location locPartida = new Location(10,10);
+        Location locChegada = new Location(50,50);
+        Passenger passageiro = new Passenger("Jorge",locPartida,locChegada);
+
+        boolean resultado = empresa.requestPickup(passageiro);
+
+        assertEquals(true,resultado,"erro");
+
+
     }
 }
