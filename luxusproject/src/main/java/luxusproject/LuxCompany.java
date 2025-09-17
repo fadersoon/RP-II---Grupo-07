a6f16edb795f1c0735a9470505d3b3b8f71d3dc1
rodrigo thoma <rodrigothdasilva@gmail.com>
2025-09-17 09:48:55 -0300

    GetVehicles modificado

--- luxusproject/src/main/java/luxusproject/LuxCompany.java
+++ luxusproject/src/main/java/luxusproject/LuxCompany.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.LinkedList;
 import java.util.Map;
 import java.util.Random;
+import java.util.Collections;
 
 public class LuxCompany {
 
@@ -86,7 +87,7 @@ public class LuxCompany {
     }
 
     public List<Vehicle> getVehicles() {
-        return vehicles;
+    return Collections.unmodifiableList(vehicles);
     }
 
     private Vehicle scheduleVehicle() {
