741c3fa8ed414fefe8e7ac17b647590faa7423cf
Gustavo Fernandes dos Anjos <gustavoanjos.aluno@unipampa.edu.br>
2025-09-17 23:23:16 -0300

    Ajuste dos Testes LuxCompanyTest

--- luxusproject/src/test/java/luxusproject/LuxCompanyTest.java
+++ luxusproject/src/test/java/luxusproject/LuxCompanyTest.java
@@ -1,17 +1,27 @@
 package luxusproject;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import static org.junit.jupiter.api.Assertions.*;
 
 
 class LuxCompanyTest {
 
+    private LuxCompany empresa;
+    private City cidade;
+
+    @BeforeEach
+
+    void setUp(){
+        this.cidade = new City(100,100);
+        this.empresa = new LuxCompany(this.cidade);
+    }
+
+
     @Test
     void testarSeACompanhiaIniciaCom4Veiculos() {
 
-        City cidade = new City(100,100);
-        LuxCompany empresa = new LuxCompany(cidade);
         assertEquals(4,empresa.getVehicles().size(),"erro");
 
 
@@ -19,8 +29,6 @@ class LuxCompanyTest {
 
     @Test
     void testarSeRequestPickupFuncionaComVeiculoLivre() {
-        City cidade = new City(100,100);
-        LuxCompany empresa = new LuxCompany(cidade);
 
         Location locPartida = new Location(10,10);
         Location locChegada = new Location(50,50);
@@ -28,8 +36,26 @@ class LuxCompanyTest {
 
         boolean resultado = empresa.requestPickup(passageiro);
 
-        assertEquals(true,resultado,"erro");
+        assertTrue(resultado,"Erro");
 
+
+    }
+
+    @Test
+    void testarSeRequestPickupFalhaQuandoTodosVeiculosEstaoOcupados(){
+
+        for(int i = 0; i < 4; i++) {
+
+            Passenger passageiroFantasma = new Passenger("Passenger" + i ,new Location(0,i),
+                    new Location(1,i));
+            empresa.requestPickup(passageiroFantasma);
+        }
+        Passenger passageiroAzarado = new Passenger("azarado", new  Location(10,10),
+                new Location(50,50));
+
+        boolean resultado = empresa.requestPickup(passageiroAzarado);
+
+        assertFalse(resultado,"O pedido deveria ter sido rejeitado, pois não há veículos livres.");
 
     }
 }
